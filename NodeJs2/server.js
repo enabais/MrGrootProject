40e060740ef9f819596fbb3589bded95a631be1a
enabais <enabais@aubay.com>
2015-05-21 13:46:50 +0200

    V3

--- NodeJs2/server.js
+++ NodeJs2/server.js
@@ -8,8 +8,11 @@ app.use(express.static(__dirname + '/'));
 io.on('connection', function(client) {
 	console.log('Client connected...');
 });
-/*app.get('/*', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});*/
+app.get('/trucs', function(req, res) {
+	mongoose.searchStatementid('temperature_3', function(err, state) {
+		res.json(state);
+
+	});
+});
 
 server.listen(8080);
